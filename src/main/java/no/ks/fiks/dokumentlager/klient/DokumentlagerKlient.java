6e1c0ff5b8212fb90308525167516d306893a953
Anders Flemmen <anders.flemmen@ks.no>
2025-04-08 09:52:15 +0200

    Dropper lukking av pushback stream

--- src/main/java/no/ks/fiks/dokumentlager/klient/DokumentlagerKlient.java
+++ src/main/java/no/ks/fiks/dokumentlager/klient/DokumentlagerKlient.java
@@ -68,7 +68,9 @@ public class DokumentlagerKlient implements Closeable {
             @NonNull UUID kontoId,
             boolean skalKrypteres
     ) {
-        try (PushbackInputStream pushbackInputStream = new PushbackInputStream(dokumentStream)) {
+        try {
+            // Not closing this, as closing the incoming stream might cause problems if it is reused, for example when reading a ZIP with multiple files using ZipArchiveInputStream
+            PushbackInputStream pushbackInputStream = new PushbackInputStream(dokumentStream);
             log.info("Skal laste opp");
             int firstByte = pushbackInputStream.read();
             if (firstByte == END_OF_STREAM) {
