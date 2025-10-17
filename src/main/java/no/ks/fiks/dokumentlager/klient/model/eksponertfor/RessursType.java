6fb670671e5a219b92f8650902655f6e24796188
carlic <carlic_01@msn.com>
2025-10-17 09:52:01 +0200

    Ryddet opp

--- src/main/java/no/ks/fiks/dokumentlager/klient/model/eksponertfor/RessursType.java
+++ src/main/java/no/ks/fiks/dokumentlager/klient/model/eksponertfor/RessursType.java
@@ -1,5 +1,5 @@
 package no.ks.fiks.dokumentlager.klient.model.eksponertfor;
 
 public enum RessursType {
-    BYGGESAKER, FAKTURA, POST, SKJEMA, ANDRESAKER
+    BYGGESAKER, FAKTURA, POST, SKJEMA, ANDRE_SAKER
 }
